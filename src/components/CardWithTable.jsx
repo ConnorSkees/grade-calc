a89455a60ba139ff06531a763829f52d9ab11233
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-01-19 02:05:00 -0500

    Move data and columns between files

--- src/components/CardWithTable.jsx
+++ src/components/CardWithTable.jsx
@@ -1,27 +1,83 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, InputNumber, Table, Divider, Card, Form, Tag, Select, Tooltip } from 'antd';
+import GradeInput from './GradeInput'
+import GradeTags from './GradeTags'
+import AssignmentNameInput from './AssignmentNameInput'
 
 const Option = Select.Option;
 
+const columns = [{
+    title: 'Assignment',
+    dataIndex: 'assignment',
+    key: 'assignment',
+    render: (text, record) => (
+      <AssignmentNameInput
+        defaultValue={record.assignment}
+      />
+    )
+  }, {
+    title: 'Points Earned',
+    dataIndex: 'pointsEarned',
+    key: 'pointsEarned',
+    render: (text, record) => (
+      <div>
+        <GradeInput
+          style={{  }}
+          pointsPossible={record.pointsPossible}
+          showGradeTag={true}
+          defaultValue={record.pointsEarned}
+          max={Math.max(record.pointsPossible, record.pointsEarned)}
+          min={0}
+        />
+      </div>
+    )
+  }, {
+    title: 'Points Possible',
+    dataIndex: 'pointsPossible',
+    key: 'pointsPossible',
+    render: (text, record) => (
+      <GradeInput defaultValue={record.pointsPossible} min={record.pointsPossible} max={record.pointsPossible} />
+    )
+  },];
+
 class CardWithTable extends Component {
-  state = {};
+  state = {
+    data: [{
+        key: '1',
+        assignment: 'Mp1 Participation Points',
+        pointsEarned: 5,
+        pointsPossible: 0,
+      },
+      {
+        key: '2',
+        assignment: 'Marking Period 1 Class Points',
+        pointsEarned: 50,
+        pointsPossible: 50,
+      },
+      {
+        key: '3',
+        assignment: 'Chapter 10 Chapter Test',
+        pointsEarned: 45,
+        pointsPossible: 54,
+      }],
+  };
 
   sumPointsEarned(){
     let sum = 0;
-    let { data } = this.props
+    let { data } = this.state;
     for (let i=0; i < data.length; i++){
-      sum += data[i]['pointsEarned']
+      sum += data[i]['pointsEarned'];
     }
-    console.log(sum)
+    console.log(sum);
     return sum;
   }
 
   sumPointsPossible(){
     let sum = 0;
-    let { data } = this.props
+    let { data } = this.state;
     for (let i=0; i < data.length; i++){
-      sum += data[i]['pointsPossible']
+      sum += data[i]['pointsPossible'];
     }
     return sum;
   }
@@ -31,7 +87,6 @@ class CardWithTable extends Component {
       <Input readOnly value={`Total Points:  ${this.sumPointsEarned()} / ${this.sumPointsPossible()}`}/>
     )
   }
-
 
   handleChange(value) {
     console.log(`selected ${value}`);
@@ -61,9 +116,10 @@ class CardWithTable extends Component {
           style={{
             width: '100%'
           }}
-          columns={this.props.columns}
-          pagination={false}
-          dataSource={this.props.data} />
+          columns={ columns }
+          pagination={ false }
+          dataSource={ this.state.data }
+        />
       </div>
     )
   };
--- src/index.js
+++ src/index.js
@@ -14,66 +14,15 @@ function reset(self) {
   self.reset();
 }
 
-const columns = [{
-    title: 'Assignment',
-    dataIndex: 'assignment',
-    key: 'assignment',
-    render: (text, record) => (
-      <AssignmentNameInput defaultValue={record.assignment} />
-    )
-  }, {
-    title: 'Points Earned',
-    dataIndex: 'pointsEarned',
-    key: 'pointsEarned',
-    render: (text, record) => (
-      <div>
-        <GradeInput
-          style={{  }}
-          ref="gradeInput"
-          pointsPossible={record.pointsPossible}
-          showGradeTag={true}
-          defaultValue={record.pointsEarned}
-          max={Math.max(record.pointsPossible, record.pointsEarned)}
-          min={0} />
-      </div>
-    )
-  }, {
-    title: 'Points Possible',
-    dataIndex: 'pointsPossible',
-    key: 'pointsPossible',
-    render: (text, record) => (
-      <GradeInput defaultValue={record.pointsPossible} min={record.pointsPossible} max={record.pointsPossible} />
-    )
-  },];
 
-const data = [{
-    key: '1',
-    assignment: 'Mp1 Participation Points',
-    pointsEarned: 5,
-    pointsPossible: 0,
-  },
-  {
-    key: '2',
-    assignment: 'Marking Period 1 Class Points',
-    pointsEarned: 50,
-    pointsPossible: 50,
-  },
-  {
-    key: '3',
-    assignment: 'Chapter 10 Chapter Test',
-    pointsEarned: 45,
-    pointsPossible: 54,
-  }
-];
+
 
 const mountNode = document.getElementById('root');
 
 
 const x = "x"
 ReactDOM.render(
-  <CardWithTable
-    columns={columns}
-    data={data}/>,
+  <CardWithTable />,
   mountNode
 );
 // ReactDOM.render(<GradeInput />, document.getElementById('root'));
