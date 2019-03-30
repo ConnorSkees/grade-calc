9d2aa1f1fb2d6a005681eedf8a807f5ba53a010f
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-03-30 18:13:31 -0400

    Remove import that no longer exists

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import CardWithTable from './components/CardWithTable'
 import FinalCalc from './components/FinalCalc'
 
 import 'antd/dist/antd.css';
